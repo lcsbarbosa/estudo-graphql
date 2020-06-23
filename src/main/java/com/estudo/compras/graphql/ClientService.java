fc2b79abd34beb3424066099e8e941f73cf89629
lcsbarbosa <lucsbarbosa7@gmail.com>
2020-06-23 12:55:43 -0300

    Creating Service

---
+++ src/main/java/com/estudo/compras/graphql/ClientService.java
@@ -0,0 +1,37 @@
+package com.estudo.compras.graphql;
+
+import com.estudo.compras.entitie.Cliente;
+import com.estudo.compras.repository.ClienteRepository;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+  @Autowired
+  private ClienteRepository repository;
+
+  public Cliente findById(Long id) {
+    return repository.findById(id).orElse(null);
+  }
+
+  public List<Cliente> findAll() {
+    return repository.findAll();
+  }
+
+  @Transactional
+  public Cliente save(Cliente client) {
+    return repository.save(client);
+  }
+
+  @Transactional
+  public Boolean deleteById(Long id) {
+    if (repository.findById(id).isPresent()) {
+      repository.deleteById(id);
+      return true;
+    }
+    return false;
+  }
+}
